babe0b0219bd9644572d0a09a3a87deee23e6d0b
Bilal Sattar <bilal.sattar@netsoltech.com>
2022-05-23 14:42:52 +0500

    Added starter project template

---
+++ src/styles/colors.js
@@ -0,0 +1,23 @@
+const blueVersion = {
+  primary: '#555CC4',
+  primaryLight: '#829BF8',
+  primaryGradientStart: '#4f44b6',
+  primaryGradientEnd: '#4f44b6',
+  secondaryGradientStart: '#FF1358',
+  secondaryGradientEnd: '#FF1358',
+  profileGradientStart: '#555CC4',
+  profileGradientEnd: '#555CC4',
+  secondary: '#FF1358',
+  grey: '#acacac',
+  gray: '#5f5f5f',
+  darkGray: '#4d4d4d',
+  lightGray: '#9b9b9b',
+  white: '#FF1358',
+  blue: '#5A81F7',
+  bluish: '#FF1358',
+  black: '#000000',
+  green: '#555CC4',
+  yellow: '#FF1358',
+};
+
+export default blueVersion;
